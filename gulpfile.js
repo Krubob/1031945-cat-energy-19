42a56d4f61e5eac76df290054737fd76adce0b62
Vladislav <res.19@yandex.ru>
2020-05-17 00:23:11 +0300

    Добвляет минификацию html

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
 var jsmin = require("gulp-jsmin");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task("css", function () {
   return gulp
@@ -86,6 +87,7 @@ gulp.task("html", function () {
   return gulp
     .src("source/*.html")
     .pipe(posthtml([include()]))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 });
 
